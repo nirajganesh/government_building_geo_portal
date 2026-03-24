e72bd915f87f71a5fc2e9326e26185b89acd02b8
nirajganesh <nirajganesh234@gmail.com>
2026-03-24 12:30:38 +0530

    Revert "update the cors proxy"
    This reverts commit 4f6acf4892a6c2bc4faeaa76ad4514da1362d6ff.

--- main.js
+++ main.js
@@ -71,15 +71,12 @@ import {
 } from './modules/building.js';
 
 // ==================== CONFIGURATION ====================
-const _isDev = import.meta.env.DEV;
-const _base  = _isDev ? '' : 'https://cggis.cgstate.gov.in';
-
 export const API_CONFIG = {
-    baseUrl:    `${_base}/giscg`,
-    adminUrl:   `${_base}/giscg/wmscgcog`,
-    wfsUrl:     `${_base}/giscg/wmscgcog`,
-    authKey:    'd01de439-448c-4b48-ac5b-5700ab0274b8',
-    apiUrl:     `${_base}/bisaglayerstatus/api/geoportal`,
+    baseUrl: 'https://cggis.cgstate.gov.in/giscg',
+    adminUrl: 'https://cggis.cgstate.gov.in/giscg/wmscgcog',
+    wfsUrl: 'https://cggis.cgstate.gov.in/giscg/wmscgcog',
+    authKey: 'd01de439-448c-4b48-ac5b-5700ab0274b8',
+    apiUrl: 'https://cggis.cgstate.gov.in/bisaglayerstatus/api/geoportal',
     apiAuthKey: '106fb8fd-ace3-4d63-8eed-d7eb2f338e274'
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,5 @@
 export default {
-    build: {
-        sourcemap: true,
-    },
-    server: {
-        proxy: {
-            '/giscg': {
-                target: 'https://cggis.cgstate.gov.in',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path
-            },
-            '/bisaglayerstatus': {
-                target: 'https://cggis.cgstate.gov.in',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path
-            }
-        }
-    }
+  build: {
+    sourcemap: true,
+  }
 }
